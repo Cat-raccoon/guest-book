12a2a18614e1aa95e8448c6a77aeb3a112b07749
raccoon <petrushko.ilya@gmail.com>
2020-06-04 23:08:52 +0300

    added 'addit' and 'delete' buttons. Started work with JS file

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var Review = function(username, usermail, revieContent){
+
+    this.username = username;
+    this.usermail = usermail;
+    this.revieContent = revieContent;
+}
+
+var review = new Review("Nick", "pochta@mail.com", "Очень важный отзыв, хоть он и тестовый, но очень важный, невероятно важный!");
