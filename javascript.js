0b63b4912a26a7c288f1d6cd97eba8df564b4ba8
raccoon <petrushko.ilya@gmail.com>
2020-07-24 11:57:19 +0300

    someShit

--- javascript.js
+++ javascript.js
@@ -4,5 +4,3 @@ var Review = function(username, usermail, revieContent){
     this.usermail = usermail;
     this.revieContent = revieContent;
 }
-
-var review = new Review("Nick", "pochta@mail.com", "Очень важный отзыв, хоть он и тестовый, но очень важный, невероятно важный!");
