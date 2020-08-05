05002e4a4e70ffabc43e1a42f6e6cdccdc450279
raccoon <petrushko.ilya@gmail.com>
2020-08-05 20:21:26 +0300

    added array 'reviews'

--- javascript.js
+++ javascript.js
@@ -1,12 +1,56 @@
-var Review = function(username, usermail, revieContent){
-
-    this.username = username;
-    this.usermail = usermail;
-    this.revieContent = revieContent;
-}
 function delFunction() {
 
     var myObj = document.getElementById("comment");
     myObj.remove();
 
 }
+
+var reviews = [1, 2, 3, 4, 5];
+
+var review1 = new Object();
+
+    review1.username = "Frodo Bagins";
+    review1.usermail = "shiir@gmail.com";
+    review1.date = "2019-5-13";
+    review1.reviewContent = "Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text.";
+
+reviews[0] = review1;
+
+var review2 = new Object();
+
+    review2.username = "Sam Smit";
+    review2.usermail = "samuel@gmail.com";
+    review2.date = "2020-5-7";
+    review2.reviewContent = "Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text.";
+
+reviews[1] = review2;
+
+var review3 = new Object();
+
+    review3.username = "Sam Smit";
+    review3.usermail = "samuel@gmail.com";
+    review3.date = "2020-5-7";
+    review3.reviewContent = "Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text.";
+
+reviews[2] = review3;
+
+var review4 = new Object();
+
+    review4.username = "Sam Smit";
+    review4.usermail = "samuel@gmail.com";
+    review4.date = "2020-5-7";
+    review4.reviewContent = "Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text.";
+
+reviews[3] = review4;
+
+var review5 = new Object();
+
+    review5.username = "Sam Smit";
+    review5.usermail = "samuel@gmail.com";
+    review5.date = "2020-5-7";
+    review5.reviewContent = "Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text.";
+
+reviews[4] = review5;
+
+
+
