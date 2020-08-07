b9dad718ba39f0016e1d1264d5b2367245d816eb
raccoon <petrushko.ilya@gmail.com>
2020-08-07 17:44:33 +0300

    addded ID for reviews

--- javascript.js
+++ javascript.js
@@ -6,59 +6,11 @@ function delFunction() {
 }
 
 var reviews = [
-    {username:"Frodo Bagins", usermail:"shiir@gmail.com", date:"2019-5-13", reviewContent:"Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text."},
-    {username:"Vladimir Putin", usermail:"russian-bear@gmail.com", date:"2020-3-10", reviewContent:"Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text."},
-    {username:"Petro Poroshenko", usermail:"chocolate_king@gmail.com", date:"2020-3-11", reviewContent:"Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text."},
-    {username:"Harry Potter", usermail:"qvidichmaster@gmail.com", date:"2020-5-5", reviewContent:"Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text."},
-    {username:"Sam Smit", usermail:"samuel@gmail.com", date:"2020-5-7", reviewContent:"Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text."}
+    {username:"Frodo Bagins", usermail:"shiir@gmail.com", date:"2019-5-13", reviewID:"111111", reviewContent:"Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text."},
+    {username:"Vladimir Putin", usermail:"russian-bear@gmail.com", date:"2020-3-10", reviewID:"111112", reviewContent:"Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text."},
+    {username:"Petro Poroshenko", usermail:"chocolate_king@gmail.com", date:"2020-3-11", reviewID:"111113", reviewContent:"Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text."},
+    {username:"Harry Potter", usermail:"qvidichmaster@gmail.com", date:"2020-5-5", reviewID:"111114", reviewContent:"Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text."},
+    {username:"Sam Smit", usermail:"samuel@gmail.com", date:"2020-5-7", reviewID:"111115", reviewContent:"Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text."}
 ];
-
-// var reviews = [1, 2, 3, 4, 5];
-
-// var review1 = new Object();
-
-//     review1.username = "Frodo Bagins";
-//     review1.usermail = "shiir@gmail.com";
-//     review1.date = "2019-5-13";
-//     review1.reviewContent = "Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text.";
-
-// reviews[0] = review1;
-
-// var review2 = new Object();
-
-//     review2.username = "Vladimir Putin";
-//     review2.usermail = "russian-bear@gmail.com";
-//     review2.date = "2020-3-10";
-//     review2.reviewContent = "Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text.";
-
-// reviews[1] = review2;
-
-// var review3 = new Object();
-
-//     review3.username = "Petro Poroshenko";
-//     review3.usermail = "chocolate_king@gmail.com";
-//     review3.date = "2020-3-11";
-//     review3.reviewContent = "Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text.";
-
-// reviews[2] = review3;
-
-// var review4 = new Object();
-
-//     review4.username = "Harry Potter";
-//     review4.usermail = "qvidichmaster@gmail.com";
-//     review4.date = "2020-5-5";
-//     review4.reviewContent = "Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text.";
-
-// reviews[3] = review4;
-
-// var review5 = new Object();
-
-//     review5.username = "Sam Smit";
-//     review5.usermail = "samuel@gmail.com";
-//     review5.date = "2020-5-7";
-//     review5.reviewContent = "Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text.  Very important text.Very important text. Very important text. Very important text. Very important text.Very important text. Very important text. Very important text. Very important text.Very important text.";
-
-// reviews[4] = review5;
-
 
 
