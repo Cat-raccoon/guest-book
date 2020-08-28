a454de6d88b5901fab042afd52b0ba337ea96b2d
raccoon <petrushko.ilya@gmail.com>
2020-08-28 22:29:34 +0300

    optimisation

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+// var items = [
+//     'Hello',
+//     'my',
+//     'name',
+//     'is',
+//     'John',
+//     'Doe'
+//   ];
+//     var string = "";
+
+//   for (i=0; i < items.length; i++) {
+//       string = string + " " + items[i];
+//   }
+//   console.log(string);
+
+
+// ______________________________________
+
+
 function delFunction() {
 
     var myObj = document.getElementsByClassName("comment")[0];
@@ -15,56 +34,10 @@ var reviews = [
 
 var comments = document.getElementById("comments");
 
-var newCom =
-  '<div class="comment"> ' +
-      '<div class="information"> ' +
-          '<p class="username">Jhony</p>' +
-          '<p class="usermail">pochta@gmail.com</p>' +
-          '<p class="date">2019-5-12</p>' +
-      '</div>' +
-      '<div class="review">' +
-          '<p class="reviewContent">' +
-          'Very important text. Very important text. Very important text.Very important text. ' +
-          'Very important text. Very important text. Very important text.Very important text. Very important text. ' +
-          'Very important text. Very important text.Very important text. Very important text. Very important text.  ' +
-          'Very important text.Very important text. Very important text. Very important text. Very important text.' +
-          'Very important text. Very important text. Very important text. Very important text.Very important text.' +
-          '</p>' +
-      '</div>' +
-      '<div class="buttons">' +
-          '<button class="edit" onclick="">Edit</button>' +
-          '<button class="delete" onclick="delFunction()">Delete</button>' +
-          '<hr class="mark">' +
-      '</div>' +
-  '</div>';
 
-//  comments.innerHTML =
-//   '<div class="comment"> ' +
-// '<div class="information"> ' +
-//       '<p class="username">Jhony</p>' +
-//       '<p class="usermail">pochta@gmail.com</p>' +
-//       '<p class="date">2019-5-12</p>' +
-//   '</div>' +
-//   '<div class="review">' +
-//       '<p class="reviewContent">' +
-//       'Very important text. Very important text. Very important text.Very important text. ' +
-//       'Very important text. Very important text. Very important text.Very important text. Very important text. ' +
-//       'Very important text. Very important text.Very important text. Very important text. Very important text.  ' +
-//       'Very important text.Very important text. Very important text. Very important text. Very important text.' +
-//       'Very important text. Very important text. Very important text. Very important text.Very important text.' +
-//       '</p>' +
-//   '</div>' +
-//   '<div class="buttons">' +
-//       '<button class="edit" onclick="">Edit</button>' +
-//       '<button class="delete" onclick="delFunction()">Delete</button>' +
-//       '<hr class="mark">' +
-//   '</div>' +
-// '</div>';
-var a = "";
+function commentTemplate(){
 
-
-for (i=0; i < reviews.length; i++) {
-    a = a + '<div class="comment"> ' +
+    return ('<div class="comment"> ' +
     '<div class="information"> ' +
         '<p class="username">Jhony</p>' +
         '<p class="usermail">pochta@gmail.com</p>' +
@@ -84,24 +57,12 @@ for (i=0; i < reviews.length; i++) {
         '<button class="delete" onclick="delFunction()">Delete</button>' +
         '<hr class="mark">' +
     '</div>' +
-'</div>';
-}
+'</div>');
 
-
+};
+var a = "";
+for (i=0; i < reviews.length; i++) {
+    a = a + commentTemplate();
+};
 comments.innerHTML = a;
-
-// var items = [
-//     'Hello',
-//     'my',
-//     'name',
-//     'is',
-//     'John',
-//     'Doe'
-//   ];
-//     var string = "";
-
-//   for (i=0; i < items.length; i++) {
-//       string = string + " " + items[i];
-//   }
-//   console.log(string);
 
